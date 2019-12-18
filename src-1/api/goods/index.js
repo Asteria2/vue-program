cd5df2c9db720e5f4177ee4f7e63b79695d0e003
asteria <1909308168@qq.com>
2019-12-18 10:44:11 +0800

    ok

--- src-1/api/goods/index.js
+++
@@ -1,6 +0,0 @@
-import axiosObj from "./interceptors.js";
-import interfaceObj from "./interface.js"
-import utilFn from "@/api/util"
-export default utilFn(axiosObj,interfaceObj)
-
-
--- src-1/api/index.js
+++
@@ -1,8 +0,0 @@
-import goodsApi from "./goods"
-import ratingsApi from "./ratings"
-import sellersApi from "./sellers"
-export default {
-    goods:goodsApi,
-    ratings:ratingsApi,
-    sellers:sellersApi
-}
